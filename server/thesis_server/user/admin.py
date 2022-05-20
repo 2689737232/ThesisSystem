0aa8588068e32797deb1eccf8d5aad1c590f9a08
zhouzhen <2689737232@qq.com>
2022-05-20 17:01:36 +0800

    fix: 修复后台修改用户和添加用户冲突

--- server/thesis_server/user/admin.py
+++ server/thesis_server/user/admin.py
@@ -5,27 +5,29 @@ from .model.PdfModel import Pdf
 from util.tools import encryption
 from django.db import transaction
 from user.views import add_user
-# Register your models here.
-# admin.site.register(Pdf)
-# admin.site.register(User)
-# admin.site.register(UserMenus)
-# admin.site.register(UserFunctions)
 
-admin.site.site_header = "论文管理后台"
-admin.site.index_title = "用户管理"
+"""
+ 判断是否是添加用户
+"""
+def is_add_user_action(request, obj):
+    # 对象为User且请求地址以 add 开头，算作是添加用户操作
+    return type(obj) is User and request.path.endswith('add')
 
-
+"""
+ 自定义admin
+"""
 class UserAdmin(admin.ModelAdmin):
     fieldsets = (
         ('用户基本信息', {'fields': ['no', 'role', 'name', 'age']}),
         ('用户密码', {'fields': ['password_bcrypt']}),
     )
 
-    # 保持模型时的操作
+    # 保存模型时的操作
     @transaction.atomic
     def save_model(self, request, obj, form, change):
         save_id = transaction.savepoint()
-        if type(obj) is User:
+        # 添加用户
+        if is_add_user_action(request, obj):
             try:
                 user: User = obj
                 add_user(user_no=user.no, password=obj.password_bcrypt,
@@ -35,9 +37,14 @@ class UserAdmin(admin.ModelAdmin):
                 if save_id is not None:
                     transaction.savepoint_rollback(save_id)
 
-        #   super().save_model(request, obj, form, change)
+        else:
+            super().save_model(request, obj, form, change)
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(UserMenus, UserAdmin)
 admin.site.register(UserFunctions, UserAdmin)
+admin.site.site_header = "论文管理后台"
+admin.site.index_title = "用户管理"
+
+
